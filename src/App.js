b32d81c743a8f1f848e62e98efd5a6c961a9ec95
ShaikAbdulBari <abdul.bari@xcubelabs.com>
2023-10-31 22:18:58 +0530

    name chnaged

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ed i t <code>src/App.js</code> and save to reload.
+          Welcome to Abdul's website
         </p>
         <a
           className="App-link"
