ece2987de56bce64087389ad571c15d1085473b5
ShaikAbdulBari <abdul.bari@xcubelabs.com>
2023-11-01 12:58:22 +0530

    name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome  Ruchika
+          Welcome  Abdul
         </p>
         <a
           className="App-link"
