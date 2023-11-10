2a2a027368c37dcd9c2437f50a63e7c41eebccef
ShaikAbdulBari <abdul.bari@xcubelabs.com>
2023-11-10 22:42:20 +0530

    pics added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import mypic from './assets/images/mypic.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={mypic} alt="My Picture" />
+    </div>
+  );
+}
+
+export default App;
