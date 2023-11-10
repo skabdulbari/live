0898628ef4e93526adb451c059f0d556c8feb1f0
ShaikAbdulBari <abdul.bari@xcubelabs.com>
2023-11-10 23:36:29 +0530

    img reomved

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css';
-import mypic from './assets/images/mypic.jpg';
 
 function App() {
   return (
     <div className="App">
-      <img src={mypic} alt="Picture not loaded" />
     </div>
   );
 }
