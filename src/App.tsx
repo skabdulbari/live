ec86cb79d4c7808421586c4baa6f44f8771793c7
ShaikAbdulBari <abdul.bari@xcubelabs.com>
2023-11-11 11:32:23 +0530

    body added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css';
-import mypic from './assets/images/mypic.jpg';
+import "./App.css";
+import Body from './componenets/Body';
+import Footer from './componenets/Footer';
+import ResponsiveAppBar from "./componenets/Navbar";
+// import mypic from './assets/images/mypic.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <img src={mypic} alt="logo" />
+    <div >
+      <div className="main-block">
+        <ResponsiveAppBar />
+        <Body/>
+        <Footer/>
+        
+      </div>
     </div>
   );
 }
---
+++ src/componenets/Body.tsx
@@ -0,0 +1,18 @@
+import { Card, Grid } from '@mui/material'
+import React from 'react'
+
+const Body = () => {
+  return (
+    <Grid>
+        <Card sx={{ minWidth: 275 }}>
+        <Grid className="right-block">
+          <Grid className="landing-screen">jhk</Grid>
+        </Grid>
+        <Grid className="left-block"></Grid>
+    </Card>
+       
+    </Grid>
+  )
+}
+
+export default Body
